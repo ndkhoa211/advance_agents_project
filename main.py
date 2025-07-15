f62c3b14a8c91710276ccc3af0bd23112b05dd5f
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-07-15 18:52:03 +0800

    create qrcode generator agent

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import PythonAstREPLTool
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+
+
 def main():
-    print("Hello from advance-agents-project!")
+    print("Start...")
+
+    instructions = """You are an agent designed to write and execute python code to answer questions.
+    You have access to a python REPL, which you can use to execute python code.
+    If you get an error, debug your code and try again.
+    Only use the output of your code to answer the question. 
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+    """
+
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+
+    prompt = base_prompt.partial(instructions=instructions)
+
+    tools = [PythonAstREPLTool()]
+
+    agent = create_react_agent(prompt=prompt,
+                               llm=ChatOpenAI(model="gpt-4.1-mini", temperature=0.0),
+                               tools=tools,
+                               )
+
+    agent_executor = AgentExecutor(agent=agent,
+                                   tools=tools,
+                                   verbose=True,
+                                   )
+
+    agent_executor.invoke(
+        input={
+            "input": """generate and save in current working directory QRcodes
+            that point to https://github.com/ndkhoa211, you have qrcode package installed already"""
+        }
+    )
 
 
 if __name__ == "__main__":
